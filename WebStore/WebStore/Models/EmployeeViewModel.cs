924c4aab25144eb4fc265386a209389c92f2c007
Арсений Жарков <shrayky@au124.ru>
2018-11-08 21:44:39 +0700

    Create 1

---
+++ WebStore/WebStore/Models/EmployeeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebStore.Models
+{
+    public class EmployeeViewModel
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDay { get; set;}
+
+    }
+}
